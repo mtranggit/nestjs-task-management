94e525b03ce267c19e81d6f60461fb9d87eb8ed5
Michael Trang <michael.trang@hotmail.com>
2021-11-21 09:32:51 +1100

    add env config

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
   UseGuards,
   Logger,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
@@ -24,7 +25,12 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   private logger = new Logger('TasksController');
 
-  constructor(private readonly tasksService: TasksService) {}
+  constructor(
+    private readonly tasksService: TasksService,
+    private readonly configService: ConfigService,
+  ) {
+    console.log(configService.get('TEST_VALUE'));
+  }
 
   @Get()
   getAllTasks(
